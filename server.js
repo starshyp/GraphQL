e3f97b58fb30e4ca006ce54ce428b6266d6191d2
emmanuelsarpong <emmanuel.sarpong12@gmail.com>
2021-07-07 09:32:31 -0400

    second commit

--- server.js
+++ server.js
@@ -309,11 +309,11 @@ function query_mysql(queryStr){
     });
 }
 
-// var app = express();
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 app.listen(4000);
 console.log('The GraphQL API server is running at localhost:4000/graphql');
